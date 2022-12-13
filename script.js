9b6787cfaea1ee1ceeed322980a8e7f73f7b1505
nate-tive <nflksrr@gmail.com>
2022-12-13 22:39:47 +0700

    add grid random color change on click

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
 let gridContainer = document.querySelector("#grid-container");
-const grids = document.createElement("div");
+let grids = document.createElement("div");
 const range = document.getElementById('size');
 const rangeLabel = document.querySelector('label');
-const gridSizes = [ 4, 8, 16, 32, 64 ];
+const gridSizes = [4, 8, 16, 32, 64];
 const contentBody = document.getElementById('main');
 
-document.getElementById('grid-size').textContent = `${gridSizes[range.value]} x ${gridSizes[range.value]}`;
-
 function updateRange() {
-    console.log(range.value);
+    // console.log(range.value);
     document.getElementById('grid-size').textContent = `${gridSizes[range.value]} x ${gridSizes[range.value]}`;
 }
-
-grids.classList.add("grid")
-
-
-gridContainer.appendChild(grids);
-
-
-let size = range.value;
-
-console.log(size, gridSizes[size]);
-
-for (let i = 1; i < gridSizes[size] * gridSizes[size]; i++) {
-    gridContainer.appendChild(grids.cloneNode());
+function randomColor(e) {
+    let color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+    // console.log(e.target);
+    e.target.style.backgroundColor = color;
 }
-gridContainer.style.gridTemplateColumns = `repeat(${gridSizes[size]}, 1fr)`;
-gridContainer.style.gridTemplateRows =  `repeat(${gridSizes[size]}, 1fr)`;
-
-function randomColor() {
-    let num = Math.floor(Math.random()*255);
-    let color = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
-    console.log(color);
-}
-
 function updateGrid() {
     size = range.value;
     gridContainer.remove();
@@ -41,13 +21,33 @@ function updateGrid() {
     newGrid.id = 'grid-container';
     contentBody.appendChild(newGrid);
     gridContainer = document.querySelector("#grid-container");
-    
+
     for (let i = 0; i < gridSizes[size] * gridSizes[size]; i++) {
-        gridContainer.appendChild(grids.cloneNode());
+        grids = document.createElement("div");
+        grids.classList.add('grid');
+        grids.addEventListener('click', randomColor);
+        gridContainer.appendChild(grids);
     }
     gridContainer.style.gridTemplateColumns = `repeat(${gridSizes[size]}, 1fr)`;
-    gridContainer.style.gridTemplateRows =  `repeat(${gridSizes[size]}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSizes[size]}, 1fr)`;
 }
 
-const allGrids = document.querySelectorAll('.grid');
-//allGrids.addEventListener(`click`, randomColor());
+document.getElementById('grid-size').textContent = `${gridSizes[range.value]} x ${gridSizes[range.value]}`;
+
+grids.classList.add("grid")
+grids.addEventListener('click', randomColor);
+
+gridContainer.appendChild(grids);
+
+let size = range.value;
+
+// console.log(size, gridSizes[size]);
+
+for (let i = 1; i < gridSizes[size] * gridSizes[size]; i++) {
+    grids = document.createElement('div');
+    grids.classList.add('grid');
+    grids.addEventListener('click', randomColor);
+    gridContainer.appendChild(grids);
+}
+gridContainer.style.gridTemplateColumns = `repeat(${gridSizes[size]}, 1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(${gridSizes[size]}, 1fr)`;
